048e2a3c45a6829f0f0ec3b263c954e96dc53cfc
Razvan Vancea <rvancea@deloittece.com>
2020-05-12 22:13:51 +0300

    update test case

--- cypress/integration/examples/DemoTest.spec.js
+++ cypress/integration/examples/DemoTest.spec.js
@@ -1,8 +1,6 @@
 describe("My first test case", () => {
   it("Navigate to the website", () => {
-    // cy.visit("http://executeautomation.com/demosite/Login.html");
     cy.visit(Cypress.env("baseUrl"));
-    console.log(Cypress.env("lorem"));
   });
 
   it("Login to app", () => {
