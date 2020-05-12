8396d392452dbafd9b7b149febce301c358dc6fd
Razvan Vancea <rvancea@deloittece.com>
2020-05-12 21:37:54 +0300

    initial commit

---
+++ cypress/integration/examples/DemoTest.spec.js
@@ -0,0 +1,19 @@
+describe("Primul meu test", () => {
+  it("Navigate to the website", () => {
+    // cy.visit("http://executeautomation.com/demosite/Login.html");
+    cy.visit(Cypress.env("baseUrl"));
+  });
+
+  it("Login to app", () => {
+    cy.get(":nth-child(1) > input").type("admin");
+    cy.get(":nth-child(2) > input").type("password");
+    cy.get(":nth-child(3) > input").click();
+  });
+
+  it("Enter user details", () => {
+    cy.get("#TitleId").select("Ms.");
+    cy.get("#Initial").type("KK");
+    cy.get("#FirstName").type("KarthiK");
+    cy.get("input[name=Hindi]").click();
+  });
+});
